9f693f0679a00e9e9fb54695e19172c5404dea46
Eddrich Janzzen Ang <eddrichang@gmail.com>
2020-03-14 23:29:42 +0800

    add unit test for db client url

---
+++ tests/test_db.py
@@ -0,0 +1,16 @@
+
+import boto3
+import moto
+import unittest
+
+from flaskr.db import get_db_client
+
+class TestDb(unittest.TestCase):
+	
+	def set_up():
+		pass
+
+	def test_get_client(self):
+		dynamodb = get_db_client()
+
+		self.assertEqual(dynamodb._endpoint.host, 'https://dynamodb.ap-southeast-2.amazonaws.com')
