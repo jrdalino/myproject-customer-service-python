e7bf7ae66c4a97fcd9f1179038edccc3e76f2d87
Eddrich Janzzen Ang <eddrichang@gmail.com>
2020-03-21 16:46:37 +0800

    remove acess_key and secret_access_key in db client

--- flaskr/db.py
+++ flaskr/db.py
@@ -4,11 +4,9 @@ import boto3
 
 def get_db_client():
 
-		dynamodb = dynamodb = boto3.client('dynamodb', 
+		dynamodb = boto3.client('dynamodb', 
 				region_name='ap-southeast-2',
-				aws_access_key_id='x',
-        aws_secret_access_key='x'
-				)
+		)
 
 		return dynamodb
 
