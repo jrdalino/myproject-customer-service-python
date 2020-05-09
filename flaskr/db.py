e16582519012472bc8ec6f60facfd356ddc55f9c
Eddrich Janzzen Ang <eddrichang@gmail.com>
2020-05-09 20:04:01 +0800

    add dynamodb configuration

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,10 +1,16 @@
 import boto3
+import os
 
 def get_db_resource():
+	# check environment as long as its not development
+	if os.environ.get("FLASK_ENV") != 'development':
+		dynamodb = boto3.resource('dynamodb')
+	else:
 		dynamodb = boto3.resource('dynamodb', 
-				region_name='ap-southeast-2',
-				# endpoint_url='http://dynamo-db:8000/',
-        		# aws_access_key_id='x',
-      			# aws_secret_access_key='x'
+			region_name='ap-southeast-2',
+			endpoint_url='http://dynamo-db:8000/',
+  		aws_access_key_id='x',
+			aws_secret_access_key='x'
 		)
-		return dynamodb
+
+	return dynamodb
